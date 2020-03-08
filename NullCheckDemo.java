afae482232f206aed98f18a533d441c29943b356
Farukh <farukh@gmail.com>
2020-03-08 14:50:33 +0530

    added file Null CheckDemo

---
+++ NullCheckDemo.java
@@ -0,0 +1,12 @@
+class NullCheckDemo{
+	public static void main(String[] args){
+		List l = new ArrayList();
+		l.add("raj");
+		l.add(100);
+		if(l != null){
+			System.out.println(l);
+
+		}
+	}
+
+}
